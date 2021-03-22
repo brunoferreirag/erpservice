0f49c4bb5fb4b24a7e00bd50e922c2d07b347452
Goncalves <bags@gft.com>
2021-03-22 14:10:11 -0300

    Refactoring

--- src/main/java/br/com/indtextbr/services/erpservice/ErpServiceApplication.java
+++ src/main/java/br/com/indtextbr/services/erpservice/ErpServiceApplication.java
@@ -2,14 +2,12 @@ package br.com.indtextbr.services.erpservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class LogisticaApplication {
+public class ErpServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LogisticaApplication.class, args);
+		SpringApplication.run(ErpServiceApplication.class, args);
 	}
 
 }
